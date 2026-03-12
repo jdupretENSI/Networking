16f98c623210da38d52051165712d5bf4a63af85
Jerome ENSI <j.dupret@nouvellesImages.xyz>
2026-03-12 16:59:13 +0100

    Struggling to add the usernames

--- Assets/Scripts/ChatManager.cs
+++ Assets/Scripts/ChatManager.cs
@@ -14,7 +14,8 @@ public class ChatManager : MonoBehaviour
 
     private void Start()
     {
-        _chatBoxManager = FindFirstObjectByType<ChatBoxManager>();
+        if (!_chatBoxManager) _chatBoxManager = FindFirstObjectByType<ChatBoxManager>();
+        
         ChatBoxManager.Instance.OnMessageSend += MessageSent;
 
         NetworkManager.Singleton.OnConnectionEvent += GetConnectedPeers;
@@ -46,14 +47,6 @@ public class ChatManager : MonoBehaviour
         string senderType = senderId == _localClientId ? "Me" : $"Player {senderId}";
         _chatBoxManager.DisplayMessage(message);
     }
-    // Get client ID properly from NetworkManager
-    private void SetLocalClientId()
-    {
-        if (NetworkManager.Singleton != null)
-        {
-            _chatBoxManager.SetCurrentUser(NetworkManager.Singleton.LocalClientId.ToString());
-        }
-    }
 
     /// <summary>
     /// Not ideal, but every time a new client connects or disconnects
@@ -63,29 +56,34 @@ public class ChatManager : MonoBehaviour
     /// <param name="newPeerId"></param>
     private void GetConnectedPeers(NetworkManager nm, ConnectionEventData ced)
     {
-        //SetLocalClientId();
-        
         if (!NetworkManager.Singleton) return;
 
         ulong localClientId = NetworkManager.Singleton.LocalClientId;
-    
-        stringContainer[] peers = NetworkManager.Singleton.ConnectedClients
+        
+        IEnumerable<ulong> allClientIds = NetworkManager.Singleton.ConnectedClients
             .Where(client 
-                => client.Key != localClientId)  // Filter out self
+                => client.Key != localClientId)
             .Select(client 
-                => new stringContainer(client.Value.ClientId.ToString()))
-            .ToArray();
+                => client.Key);
 
-        _chatBoxManager.SetupDropdownTargets(peers);
+        stringContainer[] clients = allClientIds
+            .Select(id 
+                => new stringContainer(_networkController.ClientUsernames
+                    .TryGetValue(id, out string username) 
+                    ? username : id.ToString()))
+            .ToArray();
+        
+        _chatBoxManager.SetupDropdownTargets(clients);
     }
 
-    public void SetupHostServer()
+    public void SetupHostServer() 
     {
         UnityTransport transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
         transport.ConnectionData.Address = _chatBoxManager.IP;
         transport.ConnectionData.Port = Convert.ToUInt16(_chatBoxManager.Port);
         
         NetworkManager.Singleton.StartHost();
+        _networkController.ClientUsernames.Add(NetworkManager.Singleton.LocalClientId, _chatBoxManager.CurrentPlayerName);
     }
 
     public void ClientConnectToHost()
@@ -94,9 +92,9 @@ public class ChatManager : MonoBehaviour
         UnityTransport transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
         transport.ConnectionData.Address = _chatBoxManager.IP;
         transport.ConnectionData.Port = Convert.ToUInt16(_chatBoxManager.Port);
-
-        // Connect as client
+        
         NetworkManager.Singleton.StartClient();
+        _networkController.SendPlayerJoinedServerRpc(_chatBoxManager.CurrentPlayerName);
     }
 
     public void EndChat()
--- Assets/Scripts/NetworkController.cs
+++ Assets/Scripts/NetworkController.cs
@@ -1,16 +1,19 @@
+using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
 public class NetworkController : NetworkBehaviour
 {
     private ChatManager _chatManager;
+    
+    private Dictionary<ulong, string> _clientUsernames = new();
+    public Dictionary<ulong, string>  ClientUsernames => _clientUsernames;
     private void Start()
     {
-        NetworkManager.Singleton.StartHost();
-        
         _chatManager = FindAnyObjectByType<ChatManager>();
     }
 
+    #region Messaging
     public void SendMessage(ChatBoxMessage message)
     {
         // Don't pass ID - get it from NetworkManager
@@ -34,29 +37,41 @@ public class NetworkController : NetworkBehaviour
         ulong senderId = rpcParams.Receive.SenderClientId;
         
         // Broadcast to ALL clients (including sender)
-        Debug.Log(message);
         BroadcastChatMessageClientRpc(message, senderId);
     }
 
     [ClientRpc] 
     private void BroadcastChatMessageClientRpc(ChatBoxMessage message, ulong senderId)
     {
-        Debug.Log(message);
-        // Make sure ChatManager exists
-        if (_chatManager != null)
+        if (!_chatManager) return;
+        
+        // Replace senderId with username if we have it
+        if (_clientUsernames.TryGetValue(senderId, out string username))
         {
-            Debug.Log(message);
-            _chatManager.ReceiveMessage(message, senderId);
+            message.PlayerFrom = username;
         }
-        else
-        {
-            Debug.LogError("ChatManager not found!");
-        }
+        _chatManager.ReceiveMessage(message, senderId);
     }
-    
+    #endregion
+
+    #region Utils
+
     // Helper to check if we're connected
     public bool IsConnected()
     {
         return NetworkManager.Singleton.IsConnectedClient;
     }
+    
+    [ServerRpc(RequireOwnership = false)]
+    public void SendPlayerJoinedServerRpc(string playerName, ServerRpcParams rpcParams = default)
+    {
+        ulong senderId = rpcParams.Receive.SenderClientId;
+    
+        _clientUsernames.Add(senderId, playerName);
+    }
+
+    #endregion
+    
+    
+    
 }
